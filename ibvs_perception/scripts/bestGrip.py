124e0dd4f541e2f79a44a096398815554554234f
Anas Abou Allaban <piraka9011@hotmail.com>
2019-03-15 10:44:10 -0400

    Clean up using params, launch file

--- ibvs_perception/scripts/bestGrip.py
+++ ibvs_perception/scripts/bestGrip.py
@@ -4,27 +4,31 @@ import rospy
 from gpd.msg import GraspConfigList
 from gpd.msg import GraspConfig
 
-grasps = [] # global variable to store grasps
+grasps = []  # global variable to store grasps
+
 
 def callback(msg):
     global grasps
     grasps = msg.grasps
     if len(grasps) > 0:
-        grasp = grasps[0] # grasps are sorted in descending order by score
-	print 'Selected grasp with score:', grasp.score
-	rospy.loginfo(grasp)
-	pub.publish(grasp)
+        grasp = grasps[0]  # grasps are sorted in descending order by score
+        print 'Selected grasp with score:', grasp.score
+        rospy.loginfo(grasp)
+        pub.publish(grasp)
+
 
 if __name__ == '__main__':
-	rospy.init_node('bestGrip', anonymous=True)
-	pub_topic = rospy.get_param('/ibvs/grasp_topic', '/ibvs/best_grasp')
-	pub = rospy.Publisher(pub_topic, GraspConfig)
+    rospy.init_node('bestGrip', anonymous=True)
+    pub_topic = rospy.get_param('/ibvs/grasp_topic', '/ibvs/best_grasp')
+    pub = rospy.Publisher(pub_topic, GraspConfig)
 
-	# Subscribe to the ROS topic that contains the grasps.
-	grasps_sub = rospy.Subscriber('/detect_grasps/clustered_grasps', GraspConfigList, callback)
+    # Subscribe to the ROS topic that contains the grasps.
+    grasps_sub = rospy.Subscriber('/detect_grasps/clustered_grasps', GraspConfigList,
+                                  callback)
 
-	# Wait for grasps to arrive.
-	rate = rospy.Rate(1)
+    # Wait for grasps to arrive.
+    rate = rospy.Rate(0.1)
+    while grasps_sub.get_num_connections() == 0:
+        rate.sleep()
 
-	while not rospy.is_shutdown():    
-	    rate.sleep()
+    rospy.spin()
--- ibvs_perception/scripts/publishYoloKinect.py
+++ ibvs_perception/scripts/publishYoloKinect.py
@@ -8,23 +8,26 @@ import rospy
 from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
-from perception.msg import YoloObject, YoloObjectList
+from ibvs_perception.msg import YoloObject, YoloObjectList
 from std_msgs.msg import String
 
 # Import detect_object function from training package
-sys.path.append(os.path.abspath(os.path.join(__file__,'..', '..', '..')))
+sys.path.append(os.path.abspath(os.path.join(__file__, '..', '..', '..')))
 from training.detect import YOLODetector
 
 global cv_image
 
 #TODO: Rename variables here so they are more accurate
 
+
 class YoloMock():
     def __init__(self):
         # /kinect2/sd/image_color_rect
         camera_topic = rospy.get_param('/ibvs/camera_topic',
                                        '/camera/color/image_rect_color')
-        target_topic = rospy.get_param('/ibvs/yolo_target_topic', '/ibvs/perception/yolo_target')
+        target_topic = rospy.get_param('/ibvs/yolo_target_topic',
+                                       '/ibvs/perception/yolo_target')
+
         self.pub = rospy.Publisher(target_topic, YoloObjectList)
         self.bridge = CvBridge()
         self.yolo = YoloObject()
@@ -48,60 +51,16 @@ class YoloMock():
                 obj_list.objects.append(yolo)
 
             rospy.loginfo("Object List: {}".format(obj_list))
-            #self.pub.publish(obj_list)
-            self.pub = rospy.Publisher('/amber', YoloObjectList)
             rospy.sleep(1)
             self.pub.publish(obj_list)
-
-
         except CvBridgeError as e:
             print(e)
 
-
-# class WaitForMsg():
-#     def __init__(self, topic):
-#         self.topic = topic
-#         self.timeout = None
-#         self.mutex = threading.Lock()
-#         self.subscriber = rospy.Subscriber(topic, String, self._callback, queue_size=1)
-#         self.msg = None
-#         self.execute()
-
-#     def _callback(self, msg):
-#         self.mutex.acquire()
-#         self.msg = msg
-#         self.mutex.release()
-
-#     def wait_for_msg(self):
-#         rospy.loginfo('waiting for message...')
-#         if self.timeout is not None:
-#             timeout_time = rospy.Time.now() + rospy.Duration.from_sec(self.timeout)
-#         while self.timeout is None or rospy.Time.now() < timeout_time:
-#             self.mutex.acquire()
-#             if self.msg is not None:
-#                 rospy.loginfo('got message.')
-#                 message = self.msg
-#                 if not self.latch:
-#                     self.msg = None
-
-#                 self.mutex.release()
-#                 return message
-#             self.mutex.release()
-#             rospy.sleep(.1) # TODO: InterruptException? Discuss with Anas
-
-#         rospy.loginfo(self.state_name + ' experienced a Timeout on waiting for message.')
-#         return None
-
-#     def execute(self):
-#         msg = self.wait_for_msg()
-#         if msg is not None:
-#             return msg
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
     if data.data == "start":
         ic = YoloMock()
-        #rospy.init_node('YoloMock', anonymous=True)
         r = rospy.Rate(1)
         try:
             while not rospy.is_shutdown():
@@ -111,12 +70,11 @@ def callback(data):
 
         cv2.destroyAllWindows()
 
+
 def main(args):
     rospy.init_node('YoloMock', anonymous=True)
     rospy.Subscriber("/nlp", String, callback)
     rospy.spin()
-
-    
 
 
 if __name__ == '__main__':
