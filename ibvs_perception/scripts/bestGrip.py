d87d76ab1d1a4851c6581f7728fe3e1e87d4b3ff
Anas Abou Allaban <piraka9011@hotmail.com>
2019-03-19 17:47:16 -0400

    Simulation testing setup

--- ibvs_perception/scripts/bestGrip.py
+++ ibvs_perception/scripts/bestGrip.py
@@ -5,8 +5,12 @@ from gpd.msg import GraspConfigList
 from gpd.msg import GraspConfig
 from gpd.srv import SetParameters
 from ibvs_perception.cfg import GPDwsConfig
+from sensor_msgs.msg import PointCloud2, PointField
+from std_msgs.msg import Header
 from visualization_msgs.msg import Marker, MarkerArray
+from sensor_msgs import point_cloud2
 
+import signal
 import sys
 
 
@@ -18,16 +22,28 @@ class BestGrasps:
 
         self.cfg_server = Server(GPDwsConfig, self.cfg_callback)
 
-        x_min, x_max, y_min, y_max, z_min, z_max = 0.0, 0.5, 0.0, 0.5, 0.0, 0.5
-        self.ws_marker = self.create_ws_marker(x_min, x_max, y_min, y_max, z_min, z_max)
+        self.x_min, self.x_max = -0.36, -0.06
+        self.y_min, self.y_max = -0.18, 0.15
+        self.z_min, self.z_max = 0.0, 2.0
+        self.ws_marker = self.create_ws_marker(self.x_min, self.x_max,
+                                               self.y_min, self.y_max,
+                                               self.z_min, self.z_max)
         self.marker_pub = rospy.Publisher('/visualization_marker', Marker, queue_size=5)
 
-        pub_topic = rospy.get_param('/ibvs_best_grip/grasp_topic',
-                                    '/ibvs_perception/best_grasp')
-        self.pub = rospy.Publisher(pub_topic, GraspConfig, queue_size=10)
+        grasp_topic = rospy.get_param('/ibvs_best_grip/grasp_topic',
+                                      '/ibvs_perception/best_grasp')
+        self.grasp_pub = rospy.Publisher(grasp_topic, GraspConfig, queue_size=10)
         grasp_sub = rospy.Subscriber('/detect_grasps/clustered_grasps',
                                      GraspConfigList, self.grasp_callback)
 
+        cloud_topic = '/kinect2/qhd/points'
+        pc_sub = rospy.Subscriber(cloud_topic, PointCloud2, self.pc_callback)
+        filter_cloud_topic = rospy.get_param('/ibvs_perception/filter_cloud_topic',
+                                             '/ibvs_perception/filtered_points')
+        self.pc_pub = rospy.Publisher(filter_cloud_topic, PointCloud2, queue_size=100)
+
+        rospy.loginfo("Parameters:\nGrasp Topic: {}\nCloud Topic: {}\nFiltered "
+                      "Cloud Topic: {}".format(grasp_topic, cloud_topic, filter_cloud_topic))
         # Wait for grasps to arrive.
         rate = rospy.Rate(0.1)
         while grasp_sub.get_num_connections() == 0:
@@ -42,6 +58,9 @@ class BestGrasps:
         set_ws = config.set_ws
         set_grasp_ws = config.set_grasp_ws
         set_cam_pos = config.set_cam_pos
+        self.x_min, self.x_max = config.ws_min_x, config.ws_max_x,
+        self.y_min, self.y_max = config.ws_min_y, config.ws_max_y,
+        self.z_min, self.z_max = config.ws_min_z, config.ws_max_z
         ws = [config.ws_min_x, config.ws_max_x,
               config.ws_min_y, config.ws_max_y,
               config.ws_min_z, config.ws_max_z]
@@ -64,7 +83,23 @@ class BestGrasps:
             grasp = grasps[0]  # grasps are sorted in descending order by score
             rospy.logdebug('Selected grasp with score: {}'.format(grasp.score.data))
             rospy.logdebug(grasp)
-            self.pub.publish(grasp)
+            self.grasp_pub.publish(grasp)
+
+    def pc_callback(self, msg):
+        frame_id = msg.header.frame_id
+        seq = msg.header.seq
+        gen = point_cloud2.read_points(msg, skip_nans=True)
+        new_cloud_list = []
+        for p in gen:
+            x, y, z, _ = p
+            if ((x <= self.x_max and x >= self.x_min) and
+                (y <= self.y_max and y >= self.y_min) and
+                (z <= self.z_max and z >= self.z_min)):
+                new_cloud_list.append((x, y, z))
+        pc2_header = Header(seq=seq, frame_id=frame_id, stamp=rospy.Time.now())
+        pc2_msg = point_cloud2.create_cloud_xyz32(pc2_header, new_cloud_list)
+        self.pc_pub.publish(pc2_msg)
+
 
     def create_ws_marker(self, x_min, x_max, y_min, y_max, z_min, z_max):
         dx = x_max - x_min
@@ -117,7 +152,13 @@ class BestGrasps:
                 sys.exit(0)
 
 
+def signal_handler(sig, frame):
+    print("ctrl-c captured")
+    sys.exit(0)
+
+
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
     # rospy.init_node('bestGrip', log_level=rospy.DEBUG)
     rospy.init_node('bestGrip')
     bg = BestGrasps()
