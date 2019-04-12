f45892501ed56c2d76b96c5e345dc7e062ca9b4f
Anas Abou Allaban <piraka9011@hotmail.com>
2019-04-12 08:35:42 -0400

    Docs and cleanup

--- ibvs_perception/scripts/best_grip.py
+++ ibvs_perception/scripts/best_grip.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
+"""Republish the best grasp with properly oriented transform with respect to the robot
+base.
 
+See atenpas GPD package for more info on the GraspConfig msg.
+"""
+
+# ROS
 import rospy
 from geometry_msgs.msg import TransformStamped
 from gpd.msg import GraspConfigList
@@ -7,30 +13,28 @@ from gpd.msg import GraspConfig
 from tf.transformations import quaternion_from_euler, quaternion_multiply
 import tf2_ros
 
+# Sys
 from math import pi
 import numpy as np
-from numpy.linalg import norm
 import signal
 import sys
-
-from utils import R
+import utils
+from utils import R, axis_angle, signal_handler
 
 
 class BestGrasps:
     def __init__(self):
-        self.grasps = []
-
         self.grasp_br = tf2_ros.TransformBroadcaster()
         self.grasp_br_trans = tf2_ros.TransformBroadcaster()
 
         self.camera_frame = rospy.get_param('/ibvs_perception/camera_frame',
                                             'camera_link')
-        grasp_topic = rospy.get_param('/ibvs_perception/grasp_topic',
-                                      '/ibvs_perception/best_grasp')
-        self.grasp_pub = rospy.Publisher(grasp_topic, GraspConfig, queue_size=10)
-        
-        grasp_sub = rospy.Subscriber('/detect_grasps/clustered_grasps',
-                                     GraspConfigList, self.grasp_callback)
+        new_grasp_topic = rospy.get_param('/ibvs_perception/grasp_topic',
+                                          '/ibvs_perception/best_grasp')
+        self.grasp_pub = rospy.Publisher(new_grasp_topic, GraspConfig, queue_size=10)
+
+        grasp_topic = '/detect_grasps/clustered_grasps'
+        grasp_sub = rospy.Subscriber(grasp_topic, GraspConfigList, self._grasp_callback)
 
         rospy.loginfo("Parameters:\nGrasp Topic: {}\nCamera Frame: {}".format(
             grasp_topic, self.camera_frame))
@@ -38,29 +42,15 @@ class BestGrasps:
         # Wait for grasps to arrive.
         rate = rospy.Rate(0.1)
         while grasp_sub.get_num_connections() == 0:
-            rospy.logwarn_throttle(5, "Waiting for grasps...")
+            rospy.logwarn_throttle(5, "Waiting for grasps from {}".format(grasp_topic))
             rate.sleep()
 
-    def grasp_callback(self, msg):
+    def _grasp_callback(self, msg):
         grasps = msg.grasps
         if len(grasps) > 0:
             grasp = grasps[0]  # grasps are sorted in descending order by score
             self._grasp_to_tf(grasp)
-            rospy.logdebug('Selected grasp with score: {}'.format(grasp.score.data))
-            rospy.logdebug(grasp)
             self.grasp_pub.publish(grasp)
-
-    def _unit_vector(self, v):
-        return v / norm(v)
-
-    def _axis_angle(self, axis, v):
-        axis_norm = self._unit_vector(axis)
-        v_norm = self._unit_vector(v)
-        result = np.arccos(np.clip(np.dot(axis_norm, v_norm), -1.0, -1.0))
-        if not np.isnan(result):
-            return result
-        else:
-            return 0.0
 
     def _grasp_to_tf(self, grasp):
         tf_stamped = TransformStamped()
@@ -74,15 +64,12 @@ class BestGrasps:
         tf_stamped.transform.translation.z = sample.z
 
         # Rotation
-        x_axis = np.array([1, 0, 0])
-        y_axis = np.array([0, 1, 0])
-        z_axis = np.array([0, 0, 1])
         x_grasp = np.array([grasp.axis.x, grasp.axis.y, grasp.axis.z])
         y_grasp = np.array([grasp.binormal.x, grasp.binormal.y, grasp.binormal.z])
         z_grasp = np.array([grasp.approach.x, grasp.approach.y, grasp.approach.z])
-        x_angle = self._axis_angle(-z_axis, x_grasp)
-        y_angle = self._axis_angle(y_axis, y_grasp)
-        z_angle = self._axis_angle(x_axis, z_grasp)
+        x_angle = axis_angle(-utils.Z_AXIS, x_grasp)
+        y_angle = axis_angle(utils.Y_AXIS, y_grasp)
+        z_angle = axis_angle(utils.X_AXIS, z_grasp)
         Qg = quaternion_from_euler(x_angle, y_angle, z_angle)
         Qg = quaternion_multiply(Qg, R('x', pi/2))
         tf_stamped.transform.rotation.x = Qg[0]
@@ -102,11 +89,6 @@ class BestGrasps:
                 rospy.spin()
             except KeyboardInterrupt, rospy.ROSInterruptException:
                 sys.exit(0)
-
-
-def signal_handler(sig, frame):
-    print("Ctrl-c captured")
-    sys.exit(0)
 
 
 if __name__ == '__main__':
--- ibvs_perception/scripts/yolo_bounder.py
+++ ibvs_perception/scripts/yolo_bounder.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
+"""Grabs the bounding box from a YOLO detection for a specific object, computes the
+center, then reconstructs it into 3D using a depth images and filters the point cloud
+to a sphere about the center
+
+Default object and offset in the init of YoloBounder
+"""
 
 # ROS
 import rospy
 from cv_bridge import CvBridge, CvBridgeError
-from darknet_ros_msgs.msg import BoundingBoxes, BoundingBox
+from darknet_ros_msgs.msg import BoundingBoxes
 from dialogflow_ros.msg import DialogflowResult
 from sensor_msgs import point_cloud2
 from sensor_msgs.msg import PointCloud2, Image, CameraInfo
@@ -14,23 +20,22 @@ import numpy as np
 import signal
 import sys
 import threading
+from utils import project_pixel_to_3d, rectangle_center, signal_handler
 
 
 IMG_SCALE_X = 640
 IMG_SCALE_Y = 480
-WS_X_MAX = 0.8  # 0.93
-WS_X_MIN = 0.2  # 0.16
-WS_Y_MIN = -0.16  # -0.4
-WS_Y_MAX = 0.3  # 0.31
 WS_Z_MIN = 0.00
 WS_Z_MAX = 0.65
+DF_CAPSTONE_ENTITY = 'Capstone_Entity'
+DF_GRAB_ACTION = 'grab_object'
 
 
 class YoloBounder:
-    def __init__(self):
+    def __init__(self, default_object='teddy bear', offset=0.07):
         # Params
-        self._offset = 0.07     # Shape of cube from center
-        self.desired_object = 'teddy bear'  # Desired object from Dialogflow
+        self._offset = offset     # Shape of cube from center
+        self.desired_object = default_object  # Desired object from Dialogflow
 
         # Bounding Box (BB) params
         self.xctr = 0.0     # 2D X
@@ -42,13 +47,16 @@ class YoloBounder:
         self.bridge = CvBridge()
 
         # Dialogflow Setup
-        rospy.Subscriber('/dialogflow_client/results', DialogflowResult, self._df_cb)
+        df_topic = '/dialogflow_client/results'
+        rospy.Subscriber(df_topic, DialogflowResult, self._df_callback)
 
         # Depth Image for depth info
         self.depth_img = None
-        rospy.Subscriber('/camera/depth/image_rect_raw', Image, self._img_cb)
+        depth_image_topic = '/camera/depth/image_rect_raw'
+        rospy.Subscriber(depth_image_topic, Image, self._img_callback)
         # Depth Camera Intrinsics for converstion
-        depth_info = rospy.wait_for_message('/camera/depth/camera_info', CameraInfo)
+        depth_info_topic = '/camera/depth/camera_info'
+        depth_info = rospy.wait_for_message(depth_info_topic, CameraInfo)
         self.fx = depth_info.K[0]
         self.fy = depth_info.K[4]
         self.cx = depth_info.K[2]
@@ -75,47 +83,30 @@ class YoloBounder:
         # Wait for images to arrive.
         rate = rospy.Rate(0.1)
         while self._bb_sub.get_num_connections() == 0:
-            rospy.logwarn_throttle(5, "Waiting for images...")
+            rospy.logwarn_throttle(5, "Waiting for images from {}".format(bb_topic))
             rate.sleep()
 
-    def project_pixel_to_3d(self, u, v):
-        x = (u - self.cx) / self.fx
-        y = (v - self.cy) / self.fy
-        norm = np.sqrt(x * x + y * y + 1)
-        x /= norm
-        y /= norm
-        z = 1.0 / norm
-        return np.array([x, y, z])
-
-    def compute_center(self, xmin, xmax, ymin, ymax):
-        xctr = (xmin + xmax) / 2
-        yctr = (ymin + ymax) / 2
-        if xctr > IMG_SCALE_X:
-            xctr = IMG_SCALE_X
-        if yctr > IMG_SCALE_Y:
-            yctr = IMG_SCALE_Y
-
-        return xctr, yctr
-
-    def _df_cb(self, msg):
-        if msg.action == 'grab_object':
+    def _df_callback(self, msg):
+        if msg.action == DF_GRAB_ACTION:
             parameters = msg.parameters
             for parameter in parameters:
-                if parameter.param_name == 'Capstone_Entity':
+                if parameter.param_name == DF_CAPSTONE_ENTITY:
                     self.desired_object = parameter.value[0]
                     rospy.loginfo("Updated object to: {}".format(self.desired_object))
 
-    def _img_cb(self, msg):
+    def _img_callback(self, msg):
         try:
             self.depth_img = self.bridge.imgmsg_to_cv2(msg, "16UC1")
         except CvBridgeError as e:
-            print(e)
+            rospy.logwarn("CVBridgeError exception!:\n{}".format(e))
 
         if self.bb_called:
             # Get Depth of 2D position
             depth_pt = self.depth_img[self.yctr][self.xctr]
             # Convert center to 3D normal vector
-            ctr_pt = self.project_pixel_to_3d(self.xctr, self.yctr)
+            c = (self.cx, self.cy)
+            f = (self.fx, self.fy)
+            ctr_pt = project_pixel_to_3d(self.xctr, self.yctr, c, f)
             # Change magnitude based on depth
             self.center_coordinate = ctr_pt * depth_pt
             # Scale down
@@ -149,8 +140,8 @@ class YoloBounder:
             box_obj = box.Class
             if box_obj == self.desired_object:
                 self._bb_lock.acquire()
-                self.xctr, self.yctr = self.compute_center(box.xmin, box.xmax, box.ymin,
-                                                           box.ymax)
+                self.xctr, self.yctr = rectangle_center(box.xmin, box.xmax,
+                                                        box.ymin, box.ymax)
                 self._bb_lock.release()
 
     def start_sim(self):
@@ -159,11 +150,6 @@ class YoloBounder:
                 rospy.spin()
             except KeyboardInterrupt, rospy.ROSInterruptException:
                 sys.exit(0)
-
-
-def signal_handler(sig, frame):
-    print("Ctrl-c captured")
-    sys.exit(0)
 
 
 if __name__ == '__main__':
